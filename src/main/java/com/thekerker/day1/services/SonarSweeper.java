2191ecb95b529a4fb6f31720625a28d594a90e00
Nicholas Kerker <nick.kerker@gmail.com>
2022-03-10 20:24:10 -0600

    Removed unused Slf4j annotation and import

--- src/main/java/com/thekerker/day1/services/SonarSweeper.java
+++ src/main/java/com/thekerker/day1/services/SonarSweeper.java
@@ -1,10 +1,7 @@
 package com.thekerker.day1.services;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.List;
 
-@Slf4j
 public class SonarSweeper {
 
     public int getDepthIncreases(List<Integer> measurements) {
